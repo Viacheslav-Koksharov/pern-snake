3dddd7986e773c1a7687cc2c6dfd7debaac6fa1c
Viacheslav Koksharov <v.koksharov82@gmail.com>
2022-12-12 10:41:38 +0200

    created client components

---
+++ client/src/service/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8005";
+
+export const getPlayer = () => {
+    return axios.get("/snake");
+};
+
+export const postPlayer = (name, score) => {
+    return axios.post("/snake", name, score);
+};
