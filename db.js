8ce81a9b25d4ad9b225487debb543cc692ebe309
Viacheslav Koksharov <v.koksharov82@gmail.com>
2022-12-13 14:56:34 +0200

    new fix db

--- db.js
+++ db.js
@@ -8,17 +8,11 @@ require("dotenv").config();
 //     port: process.env.PG_PORT,
 //     database: process.env.PG_DATABASE,
 // }
-// const devconfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.
-//     PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const devconfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.
+    PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-// const proConfig = process.env.DATABASE_URL; //heroku addons
+const proConfig = process.env.POSTGRESQL_EXTERNAL_URL;
 
-// const pool = new Pool({ connectionString: process.env.NODE_ENV === "production" ? proConfig : devconfig });
-const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-})
+const pool = new Pool({ connectionString: process.env.NODE_ENV === "production" ? proConfig : devconfig });
+
 module.exports = pool;
