01ce1ffbcf1f9b73d3d33dad7006465d5047e050
Viacheslav Koksharov <v.koksharov82@gmail.com>
2022-12-13 14:31:25 +0200

    fix db else

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 8005;
 app.use(cors());
 app.use(express.json()); //req.body
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static("./client/build"));
+app.use(express.static("./build"));
 
 if (process.env.NODE_ENV === "production") {
     //server static component
     //npm run build
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "build")));
 }
 
 //ROUTES//
