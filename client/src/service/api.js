acbdddae3d0c6f1d1f88d6e7a974618b359a1e38
Petrovich <64725709+Viacheslav-Koksharov@users.noreply.github.com>
2022-12-23 12:46:37 +0200

    fix package.json

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:5000";
-axios.defaults.baseURL = "https://snake-api.onrender.com";
+axios.defaults.baseURL = 'https://snake-api.onrender.com';
 
 export const getPlayer = () => {
-    return axios.get("/snake");
+    return axios.get('/snake');
 };
 
 export const postPlayer = (name, score) => {
-    return axios.post("/snake", name, score);
+    return axios.post('/snake', name, score);
 };
