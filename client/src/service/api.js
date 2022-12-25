606f168075f15b6596670ccf6e1aa8776682acf7
Petrovich <64725709+Viacheslav-Koksharov@users.noreply.github.com>
2022-12-25 23:08:49 +0200

    change url

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
-
+// axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://pern-snake-api.herokuapp.com/";
 const getPlayer = () => {
     return axios.get("/snake");
 };
